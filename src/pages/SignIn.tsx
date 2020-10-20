ba9d07b489a0fd1ef213f823c98f8eec97540a4c
Amelit-Q <amelitstanding@gmail.com>
2020-10-20 19:56:23 +0300

    Working with CSS for now and doing Login page.

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,76 @@
+import { Button, makeStyles, Typography } from '@material-ui/core';
+import React from 'react';
+import { theme } from '../theme';
+import './../styles/signin.scss';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    display: 'flex',
+    height: '100vh',
+  },
+  blueSide: {
+    flex: '0 0 50%',
+    backgroundColor: '#1DA1F2',
+  },
+  loginSide: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: '0 0 50%',
+    overflow: 'hidden',
+  },
+  loginSideTwitterIcon: {
+    fontSize: 45,
+  },
+  loginSideWrapper: {
+    width: 380,
+  },
+  loginSideTitle: {
+    fontWeight: 700,
+    fontSize: 32,
+    marginBottom: 60,
+    marginTop: 25,
+  },
+}));
+
+export const SignIn = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <section className={classes.blueSide}>
+        <ul>
+          <li>
+            <Typography>Follow your interests.</Typography>
+          </li>
+          <li>
+            <Typography>Hear what people are talking about</Typography>
+          </li>
+          <li>
+            <Typography>Join the conversation.</Typography>
+          </li>
+        </ul>
+      </section>
+
+      <section className={classes.loginSide}>
+        <div className={classes.loginSideWrapper}>
+          <TwitterIcon color="primary" className={classes.loginSideTwitterIcon} />
+          <Typography variant="h4" className={classes.loginSideTitle}>
+            See what’s happening in the world right now
+          </Typography>
+          <Typography>
+            <b>Join Twitter today.</b>
+          </Typography>
+          <br />
+          <Button style={{ marginBottom: 10 }} variant="contained" color="primary" fullWidth>
+            Sign Up
+          </Button>
+          <Button variant="outlined" fullWidth>
+            Log in
+          </Button>
+        </div>
+      </section>
+    </div>
+  );
+};
